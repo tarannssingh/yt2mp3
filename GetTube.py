798b2bd5f5f3642f0577b4abff6737d53c63c856
Taran Singh <tarannssingh@gmail.com>
2024-03-25 22:56:45 -0700

    feat: fixed audio bug

--- GetTube.py
+++ GetTube.py
@@ -41,31 +41,55 @@ if not os.path.exists(PATH):
     os.mkdir(PATH)
 
 if PATH == "./music":
+    
+    # technology if we use youtube for link 
     t = v.streams.filter(only_audio=True).all()
     t[0].download(PATH)
-    print(f"{PATH}/{os.getcwd()}.mp4")
-    video = mp.VideoFileClip(f"./{PATH}/{v.title}.mp4")
-    clip = video.subclip(0, video.duration)
-    clip.audio.write_audiofile(f"{PATH}/{v.title}.mp3")
+    os.chdir(f"{PATH}")
+    video = mp.AudioFileClip(f"{v.title}.mp4").set_duration(v.length) #.without_silence()
+    video = video.remove_dead_audio(min_silence_length=10, silence_thresh=-30) 
+    video.write_audiofile(f"{v.title}.mp3")
+    # audio_path = os.path.join(PATH, f"{v.title}.mp3")  # Assuming you want to save as MP3
     
+    os.chdir(os.path.join(os.curdir, ".."))
+
+
 if PATH == "./videos":
     v.streams.get_highest_resolution().download(PATH)
+    audio = v.streams.filter(only_audio=True).all()[0]
+
+    # get the audio clip
+    audio = v.streams.filter(only_audio=True).all()[0].download(PATH, filename="test.mp4")
+    os.chdir(f"{PATH}")
+    # this is to properlly set the audio duration
+    audio = mp.AudioFileClip(f"test.mp4").set_duration(v.length) #.without_silence()
+    audio.write_audiofile(f"test.mp3") 
+
+    # audio.write_audiofile(f"test.mp3")
+
+    # combine the audio clip and video clip
+    video = mp.VideoFileClip(f"{v.title}.mp4")
+    audioclip = mp.CompositeAudioClip([mp.AudioFileClip(f"test.mp3")])
+    video.audio = audioclip
+    video.write_videofile(f"{v.title}-video.mp4")
+
+    os.chdir(os.path.join(os.curdir, ".."))
+    
+    # print(f"{os.getcwd()}{PATH[1:]}/{v.title}.mp4")
 
 
 from pytube import Channel
 
 # Instantiate a Channel object with the YouTube channel URL
-c = Channel("https://www.youtube.com/c/SidhuMooseWalaOfficial/videos")
-
+# c = Channel("https://www.youtube.com/c/SidhuMooseWalaOfficial/videos")
 # Iterate through video URLs in the channel and print them
-for url in c.video_urls:
-    print(url)
+# for url in c.video_urls:
+#     print(url)
 
 # It seems you're attempting to download a video and print its title, 
 # but you haven't defined 'v' in your code snippet.
 # Assuming you want to download the first video from the channel:
-video_url = c.video_urls[0]  # Get the URL of the first video
-v = c.get_video(video_url)   # Get the video object
-v.download()                 # Download the video
-print(f"Downloaded {v.title} successfully")
+# video_url = c.video_urls[0]  # Get the URL of the first video
+# v = c.get_video(video_url)   # Get the video object
+# v.download()                 # Download the video
 print(f"Downloaded {v.title} successfully")
